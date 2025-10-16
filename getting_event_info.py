396d1429e18a70c49573b608bc3de62437664b0c
BARATZL <bratzlaff00@gmail.com>
2025-10-16 11:30:42 -0400

    renaming for clarity and adding more formal return output

--- getting_event_info.py
+++ getting_event_info.py
@@ -11,8 +11,11 @@ SCOREBOARD_URL = "https://site.api.espn.com/apis/site/v2/sports/football/college
 def get_event_ids(yyyymmdd:str):
     url = f"{SCOREBOARD_URL}?dates={yyyymmdd}"
     reponse = requests.get(url)
+    data_pull = {}
     if response.ok:
         data = response.json()
+        season = data['leagues'][0]['season']['year']
+        week = data['events'][0]['week']['number']
         event_ids = [event['id'] for event in data['events']]
         if len(event_ids) < 1:
             return []
@@ -20,4 +23,9 @@ def get_event_ids(yyyymmdd:str):
         print(event_ids)
     else:
         raise ValueError("Non 200 response.")
-    return event_ids
+
+    data_pull['event_id'] = event_ids
+    data_pull['week'] = week
+    data_pull['season'] = season
+    
+    return data_pull
