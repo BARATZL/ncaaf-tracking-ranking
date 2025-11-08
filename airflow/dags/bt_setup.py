4feca2336383dd993a22df2d9dbe621d85762152
BARATZL <bratzlaff00@gmail.com>
2025-11-08 15:56:49 -0500

    Create bt_setup.py

---
+++ airflow/dags/bt_setup.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+from airflow.sdk import dag, task    
+from pathlib import Path
+import duckdb
+import os
+from ncaaf import utils
+
+# paths, as the airflow project is a project we deploy to astronomer
+BASE_DIR = Path(os.environ.get("AIRFLOW_HOME", "/usr/local/airflow"))
+SQL_DIR = BASE_DIR / "include" / "sql"
+
+@dag(
+    schedule=None,
+    start_date=datetime(2025, 11, 6),
+    catchup=False,
+    tags=["bt", "setup"]
+)
+def bt_setup():
+
+    @task
+    def setup_schema():
+        s = utils.read_sql(SQL_DIR / "bt-schema.sql")
+        utils.run_execute(s)
+    
+    setup_schema()
+
+bt_setup()
