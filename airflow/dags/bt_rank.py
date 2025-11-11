6cbcb12ba700e10c4fcaa1515d750ed12639ec4a
Barrett <baratz00@bu.edu>
2025-11-11 02:48:03 +0000

    adding ranking DAG from Shawn

---
+++ airflow/dags/bt_rank.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+from airflow.decorators import dag, task
+import requests
+
+CF_URL = "https://us-central1-baratz00-ba882-fall25.cloudfunctions.net/bradley-terry-rankings"
+
+@dag(
+    dag_id="bt_rankings_weekly",
+    schedule="0 22 * * 2", # every tuesday 10pm
+    start_date=datetime(2025, 1, 1),
+    catchup=False, # no back fill
+    tags=["bt", "ranking", "motherduck"],
+)
+def bt_rankings_weekly():
+
+    @task
+    def call_bt_function():
+        resp = requests.get(CF_URL, timeout=60)
+        resp.raise_for_status()        # know the status
+        data = resp.json()
+        print(data)
+        return data
+
+    call_bt_function()
+
+bt_rankings_weekly()
